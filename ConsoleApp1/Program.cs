b57b4921435d4881b045f17d2dc36fe22daf3aa5
danil <dddd@gmail.com>
2024-02-01 00:59:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿double f (double nn)
+{
+    double x = 1;
+    for (int i = 1; i <= nn; i++)
+        x = x * i;
+    return x;
+}
+double s (double a, double k)
+{
+    double x=1;
+    for(int i = 1;i <= k; i++)
+        x = x * a;
+    return x;
+}
+Console.WriteLine("введите x,y,a,b,k,i");
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double k = double.Parse(Console.ReadLine());
+double i = double.Parse(Console.ReadLine());
+double y1 = (Math.Sqrt(s(a, k) - s(b, i)) / (x + b) + f(y + a));
+Console.WriteLine($"y ={y1}");
---
+++ laba 6/Program.cs
@@ -0,0 +1,36 @@
+﻿static void Main(string[] args)
+{
+    void Factorial(double nn, out double x)
+    {
+        x = 1;
+        for (int i = 1; i < nn; i++)
+        {
+            x = x * i;
+        }
+    }
+    void Stepen(double A, double K, out double x)
+    {
+        x = 1;
+        for (int i = 1; i < K; i++)
+        {
+            x *= A;
+        }
+    }
+    Console.WriteLine("a=");
+    double a = double.Parse(Console.ReadLine());
+    Console.WriteLine("b=");
+    double b = double.Parse(Console.ReadLine());
+    Console.WriteLine("k=");
+    double k = double.Parse(Console.ReadLine());
+    Console.WriteLine("l=");
+    double l = double.Parse(Console.ReadLine());
+    Console.WriteLine("x=");
+    double X = double.Parse(Console.ReadLine());
+    Console.WriteLine("y=");
+    double y = double.Parse(Console.ReadLine());
+    Stepen(a, k, out double S1);
+    Stepen(b, l, out double S2);
+    Factorial(y + a, out double p1);
+    double F = Math.Sqrt(S1 - S2) / (X + b) + p1;
+    Console.WriteLine($"F= {F}");
+}
